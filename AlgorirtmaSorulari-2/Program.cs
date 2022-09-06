562e39f1c19b352b79a2cce64a5cdc124386ff1a
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2022-09-06 20:02:57 +0300

    Add project files.

---
+++ AlgorirtmaSorulari-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorirtmaSorulari_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[20];
+            int[] enBuyukler = new int[3];
+            int[] enKucukler = new int[3];
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("{0}. Sayıyı giriniz:", i + 1);
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                sayilar[i] = sayi;
+            }
+
+            Array.Sort(sayilar);
+            for (int j = 0; j < 3; j++)
+            {
+                enKucukler[j] = sayilar[j];
+            }
+
+            Array.Reverse(sayilar);
+            for (int j = 0; j < 3; j++)
+            {
+                enBuyukler[j] = sayilar[j];
+            }
+
+            Console.WriteLine($"En büyüklerin ortalaması:{Math.Round(enBuyukler.Average(), 2)}");
+            Console.WriteLine($"En küçüklerin ortalaması:{Math.Round(enKucukler.Average(), 2)}");
+            Console.WriteLine($"Ortalamaların toplamı {Math.Round((enBuyukler.Average() + enKucukler.Average()), 2)}");
+           
+        }
+    }
+}
---
+++ baslangıc/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace baslangıc
+{// Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
+// Negatif ve numeric olmayan girişleri engelleyin.
+// Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+// Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   float asalav=0;
+            float normalav = 0;
+            ArrayList asalsayi = new ArrayList();
+            ArrayList normalsayi=new ArrayList();
+            int kontrol = 0;
+
+            for (int i = 0; i <20; i++)
+            {
+                Console.WriteLine("{0}.sayıyı giriniz", i+1);
+                bas:
+                string sayi = Console.ReadLine();
+                int sayii = Convert.ToInt32(sayi);
+                for (int j = 0; j < sayii; j++)
+                {
+                    if (sayii%j==0)
+                    {
+                        kontrol++;
+
+                    }                   
+                }
+                if (kontrol==0)
+                {
+                    asalsayi.Add(sayii);
+                }
+                else if(kontrol!=0)              
+                {
+                    normalsayi.Add(sayii);
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış değer {0}. sayı hatalı",i+1);
+                    goto bas;
+                }
+            }
+
+            asalsayi.Sort();
+            asalsayi.Reverse();
+            
+            normalsayi.Sort();
+            normalsayi.Reverse();
+
+            Console.WriteLine("****Aasal olmayan sayılar****");
+            foreach (var normal in normalsayi)
+            {
+                Console.WriteLine(normal);
+                normalav += Convert.ToInt32(normal);
+            }
+            Console.WriteLine($"Toplam {normalsayi.Count} normal sayı gırısı yapılmıstır ve ortalaması {normalav / normalsayi.Count} ");
+
+            foreach (var asal in asalsayi)
+            {
+                Console.WriteLine(asal);
+                asalav += Convert.ToInt32(asal);
+            }
+            Console.WriteLine($"Toplam {asalsayi.Count} normal sayı gırısı yapılmıstır ve ortalaması {asalav / asalsayi.Count} ");
+        }
+
+        }
+    }
+}
